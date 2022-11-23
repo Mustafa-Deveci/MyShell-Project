140085a9db59923b5add8e3e1d2a0b7076466b16
Mustafa Deveci <mustafa.deveci@stu.fsm.edu.tr>
2022-11-23 22:25:50 +0300

    Add files via upload

---
+++ myShell.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char input[100];
+
+
+int main(){
+    
+    int i;
+    
+    while(1) {
+        
+        
+        printf("myshell>>");
+        
+        getline(&satir, &bufsize, stdin); //satır okur.
+        
+        if(words != NULL){
+            
+            if(strcmp(input,"exit")==0){
+                break;
+            }
+            else if(strcmp(input ,"clear")==0){
+                
+            }
+            else if(strcmp(input ,"help")==0){
+                
+                
+            }
+            else if(strcmp(words[0],"cat")==0){
+                
+            }
+        }
+        
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
