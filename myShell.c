2f8ce0cc55caa9c877c251c2423395fe2dd532a1
Mustafa Deveci <mustafa.deveci@stu.fsm.edu.tr>
2022-11-26 22:18:10 +0300

    Add files via upload

--- myShell.c
+++ myShell.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 char input[500];
 
 int main(int argc,char *argv[])
 {
-
+    time_t t;
+    system("clear");
+    t = time( NULL );
+    printf( "\nTime: %s\n", ctime( &t ));
     while(1){
         
         printf("myshell>>");
@@ -15,7 +19,13 @@ int main(int argc,char *argv[])
         getchar();
         
         if(strcmp(input,"help")==0){
-                
+                      printf("\nOperations:\n");
+                      printf("cat - cat komutu sonrası yazdığınız şeyleri yazdırır.\n");
+                      printf("clear - ekranı temizler \n");
+                      printf("ls - klasör içindeki dosyaları görebilirsiniz.\n");
+                      printf("exit - myShellden çıkış işlemi.\n");
+                      printf("bash - sistemdeki bash programı çağrılır, bashteki normal işlemleri yapabilirsiniz, myshell için exit yazmanız yeterlidir\n");
+                      printf("\n");
 
               }
         else if(strcmp(input,"bash")==0){
@@ -28,10 +38,13 @@ int main(int argc,char *argv[])
             system("clear");
         }
         else if (strcmp(input, "exit")==0){
+            t = time( NULL );
+            printf( "\nTime: %s\n", ctime( &t ));
             break;
         }
-        else if((input, "cat" ) == input) {
-
+        else if(strstr(input, "cat" ) == input) {
+            printf("%s ",input);
+            printf("\n");
         }
                else{
             printf("Yanlış bir komut girdiniz, yardım için 'help' komutunu girebilirsiniz\n");
